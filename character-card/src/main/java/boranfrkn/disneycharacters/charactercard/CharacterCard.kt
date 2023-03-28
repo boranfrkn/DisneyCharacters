0bff0c15c309ea69429fd5371ca76b5701f2143a
boranfrkn <boranfrkn@gmail.com>
2023-03-28 06:39:29 +0300

    create sample character card

--- character-card/src/main/java/boranfrkn/disneycharacters/charactercard/CharacterCard.kt
+++ character-card/src/main/java/boranfrkn/disneycharacters/charactercard/CharacterCard.kt
@@ -1,19 +1,54 @@
 package boranfrkn.disneycharacters.charactercard
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 
 @Composable
-fun CharacterCard() {
-    Card {
-        Text("Card")
+fun CharacterCard(
+    imageUrl: String,
+    name: String
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(4.dp),
+        shape = RoundedCornerShape(8.dp)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+        ) {
+            Card(
+                shape = RoundedCornerShape(8.dp)
+            ) {
+                AsyncImage(
+                    model = imageUrl,
+                    contentDescription = "",
+                    contentScale = ContentScale.FillBounds,
+                    modifier = Modifier.size(100.dp)
+                )
+            }
+            Spacer(modifier = Modifier.size(8.dp))
+            Text(text = name, modifier = Modifier.align(Alignment.CenterVertically))
+        }
     }
 }
 
 @Preview
 @Composable
 fun CharacterCardPreview() {
-    CharacterCard()
+    CharacterCard("imageUrl", "Test Name")
 }
--- characters/src/main/java/boranfrkn/disneycharacters/characters/MainActivity.kt
+++ characters/src/main/java/boranfrkn/disneycharacters/characters/MainActivity.kt
@@ -3,51 +3,38 @@ package boranfrkn.disneycharacters.characters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.compose.collectAsLazyPagingItems
 import boranfrkn.disneycharacters.charactercard.CharacterCard
 import boranfrkn.disneycharacters.theme.DisneyCharactersTheme
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: CharactersViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             DisneyCharactersTheme {
+                val state = viewModel.characters.value.collectAsLazyPagingItems()
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Column {
-                        CharacterCard()
-                        Greeting("Android")
-                    }
+                    LazyColumn(content = {
+                        items(state.itemCount) {
+                            CharacterCard(imageUrl = state[it]?.imageUrl.orEmpty(), name = state[it]?.name.orEmpty())
+                        }
+                    })
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DisneyCharactersTheme {
-        CharacterCard()
-        Greeting("Android")
     }
 }
