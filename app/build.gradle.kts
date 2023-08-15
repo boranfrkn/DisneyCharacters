c573c0b0ee80dd5c3ecab6c62aa5abbc74745e79
boranfrkn <boranfrkn@gmail.com>
2023-08-16 00:04:18 +0300

    update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "boranfrkn.disneycharacters"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
